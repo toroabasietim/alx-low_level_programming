b2ea6d5624fbfdd0591d2f1b25b4e9a56e515898
toroabasietim <toroabasietim909@gmail.com>
2022-09-11 19:02:41 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,45 +1,14 @@
 #include <stdlib.h>
-
 #include <time.h>
+/* more headers goes there */
 
-#include <stdio.h>
-
-
-
-/**
-
- * main - assigns a random number to int n everytime
-
- * it executes, and prints it
-
- * Return: Always 0 (Success)
-
- */
-
+/* betty style doc for function main goes there */
 int main(void)
-
 {
+        int n;
 
-	int n;
-
-
-
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-
-		printf("%d is positive\n", n);
-
-	else if (n == 0)
-
-		printf("%d is zero\n", n);
-
-	else if (n < 0)
-
-		printf("%d is negative\n", n);
-
-	return (0);
-
+        srand(time(0));[D[D[D[D
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        return (0);
 }
